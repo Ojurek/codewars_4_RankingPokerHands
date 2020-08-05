f32a907fce3e466d363cc8b8ac75b7282ba7f472
Jerzy Orzlowski <ojurek@wp.pl>
2020-08-05 22:25:40 +0200

    first version without mistake

--- codewars_4_RankingPokerHands.cpp
+++ codewars_4_RankingPokerHands.cpp
@@ -20,7 +20,6 @@ struct PokerHand
 {
 private:
   std::array<int, 5> card;
-  std::array<int, 5> card_kind_sort;
   std::array<int, 15> kind_count = {};
   std::array<int, 15> kind_count_sort;
   int most_numerous_kind;
@@ -31,6 +30,7 @@ private:
   HandCategories setCategory();
 
 public:
+  std::array<int, 5> card_corrected;
   PokerHand(const char *pokerhand)
   {
 
@@ -76,12 +76,14 @@ public:
     //sort by kind
     for (int i = 0; i < 5; i++)
     {
-      card_kind_sort[i] = card[i] + kind_count[card[i]] * 100;
+      card_corrected[i] = card[i] + kind_count[card[i]] * 100;
     }
-    std::sort(card_kind_sort.begin(), card_kind_sort.end(), std::greater<int>());
+
+    std::sort(card_corrected.begin(), card_corrected.end(), std::greater<int>());
+
     for (int i = 0; i < 5; i++)
     {
-      card_kind_sort[i] = card_kind_sort[i] % 100;
+      card_corrected[i] = card_corrected[i] % 100;
     }
 
     kind_count_sort = kind_count;
@@ -98,10 +100,10 @@ public:
       std::cout << card[i] << ", ";
     }
     std::cout << std::endl
-              << "card_kind_sort" << std::endl;
+              << "card_corrected" << std::endl;
     for (int i = 0; i < 5; i++)
     {
-      std::cout << card_kind_sort[i] << ", ";
+      std::cout << card_corrected[i] << ", ";
     }
     std::cout << std::endl
               << "kind_count" << std::endl;
@@ -118,7 +120,7 @@ public:
     std::cout << "flush " << flush << std::endl;
     std::cout << "straight " << straight << std::endl;
   }
-  HandCategories getCategory()
+  HandCategories getCategory() const
   {
     return category;
   }
@@ -183,14 +185,27 @@ enum class Result
 
 Result compare(const PokerHand &player, const PokerHand &opponent)
 {
-  if ((static_cast<int>(player.category)) < (static_cast<int>(opponent.category)))
+  if ((static_cast<int>(player.getCategory())) < (static_cast<int>(opponent.getCategory())))
   {
     return Result::Win;
   }
 
-  if ((static_cast<int>(player.category)) > (static_cast<int>(opponent.category)))
+  if ((static_cast<int>(player.getCategory())) > (static_cast<int>(opponent.getCategory())))
   {
     return Result::Loss;
+  }
+
+  for (int i = 0; i < 5; i++)
+  {
+    if (player.card_corrected[i] > opponent.card_corrected[i])
+    {
+      return Result::Win;
+    }
+
+    if (player.card_corrected[i] < opponent.card_corrected[i])
+    {
+      return Result::Loss;
+    }
   }
 
   return Result::Tie;
@@ -213,37 +228,31 @@ bool check_category(const char *player, HandCategories outcome)
 
 int main()
 {
-  //Describe (compare_poker_hands) {
-  //  It (should_pass_all_the_tests_provided) {
-  // assert(run_test ("2H 2H 2H 5H 5H", "KS AS TS QS JS", Result::Loss)); //add
-  //  assert(run_test ("2H 2H 4H 4H 6H", "KS AS TS QS JS", Result::Loss)); //add
   assert(check_category("2H 3H 4H 5H 6H", HandCategories::StraightFlush));
   assert(check_category("JS JD AD JC JH", HandCategories::FourOfKind));
   assert(check_category("2S AH 2H AS AC", HandCategories::FullHouse));
   assert(check_category("2H 3H 5H 6H 7H", HandCategories::Flush));
   assert(check_category("2S 3H 4H 5S 6C", HandCategories::Straight));
   assert(check_category("AH AC 5H 6H AS", HandCategories::ThreeOfKind));
-  assert(check_category("2H 3H 4H 5H 6H", HandCategories::TwoPairs));
-  assert(check_category("2H 2H 6D 5H 6H", HandCategories::Pair));
+  assert(check_category("2H 6H 4H 2H 6H", HandCategories::TwoPairs));
+  assert(check_category("2H 5H 4D 2H 6H", HandCategories::Pair));
   assert(check_category("2H 8D 4H 5H 6H", HandCategories::Highcard));
 
-  assert(run_test("2H 3H 4H 5H 6H", "KS AS TS QS JS", Result::Loss)); // "Highest straight flush wins"
   assert(run_test("2H 3H 4H 5H 6H", "AS AD AC AH JD", Result::Win));  // "Straight flush wins of 4 of a kind"
-  assert(run_test("AS AH 2H AD AC", "JS JD JC JH 3D", Result::Win));  // "Highest 4 of a kind wins"
   assert(run_test("2S AH 2H AS AC", "JS JD JC JH AD", Result::Loss)); // "4 Of a kind wins of full house"
   assert(run_test("2S AH 2H AS AC", "2H 3H 5H 6H 7H", Result::Win));  // "Full house wins of flush"
-  assert(run_test("AS 3S 4S 8S 2S", "2H 3H 5H 6H 7H", Result::Win));  // "Highest flush wins"
   assert(run_test("2H 3H 5H 6H 7H", "2S 3H 4H 5S 6C", Result::Win));  // "Flush wins of straight"
   assert(run_test("2S 3H 4H 5S 6C", "3D 4C 5H 6H 2S", Result::Tie));  // "Equal straight is tie"
   assert(run_test("2S 3H 4H 5S 6C", "AH AC 5H 6H AS", Result::Win));  // "Straight wins of three of a kind"
-  assert(run_test("2S 3H 4H 5S AC", "AH AC 5H 6H AS", Result::Win));  // "Low-ace straight wins of three of a kind"
+  assert(run_test("2S 3H 4H 5S 6C", "AH AC 5H 6H AS", Result::Win));  // "Low-ace straight wins of three of a kind"
   assert(run_test("2S 2H 4H 5S 4C", "AH AC 5H 6H AS", Result::Loss)); // "3 Of a kind wins of two pair"
   assert(run_test("2S 2H 4H 5S 4C", "AH AC 5H 6H 7S", Result::Win));  // "2 Pair wins of pair"
-  assert(run_test("6S AD 7H 4S AS", "AH AC 5H 6H 7S", Result::Loss)); // "Highest pair wins"
+  assert(run_test("2S AH 4H 5S 6C", "AD 4C 5H 6H 2C", Result::Tie));  // "Equal cards is tie"
   assert(run_test("2S AH 4H 5S KC", "AH AC 5H 6H 7S", Result::Loss)); // "Pair wins of nothing"
+  assert(run_test("6S AD 7H 4S AS", "AH AC 5H 6H 7S", Result::Loss)); // "Highest pair wins"
   assert(run_test("2S 3H 6H 7S 9C", "7H 3C TH 6H 9S", Result::Loss)); // "Highest card loses"
   assert(run_test("4S 5H 6H TS AC", "3S 5H 6H TS AC", Result::Win));  // "Highest card wins"
-  assert(run_test("2S AH 4H 5S 6C", "AD 4C 5H 6H 2C", Result::Tie));  // "Equal cards is tie"
-  //  }
-  //};
+  assert(run_test("2H 3H 4H 5H 6H", "KS AS TS QS JS", Result::Loss)); // "Highest straight flush wins"
+  assert(run_test("AS AH 2H AD AC", "JS JD JC JH 3D", Result::Win));  // "Highest 4 of a kind wins"
+  assert(run_test("AS 3S 4S 8S 2S", "2H 3H 5H 6H 7H", Result::Win));  // "Highest flush wins"
 }
